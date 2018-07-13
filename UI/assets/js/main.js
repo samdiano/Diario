d7ff3ae428e28011f1a6004b70b00bd36900a3c8
Samuel George <samuegeorge58@gmail.com>
2018-07-13 16:01:21 +0000

    Revert "Create UI template for the app"

--- UI/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-const toggle = () => {
-    const nav = document.getElementById("myTopnav");
-    if (nav.className === "topnav") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "topnav";
-    }
-}
