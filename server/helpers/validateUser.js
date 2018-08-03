8fb5fe73098baa3f3816d96577108059010b99cd
Samuel George <samuegeorge58@gmail.com>
2018-08-03 10:22:34 +0100

    remove status

--- server/helpers/validateUser.js
+++ server/helpers/validateUser.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const validateUser = (user) => {
   const schema = {
-    full_name: Joi.string().min(5)..trim()max(255).required(),
+    full_name: Joi.string().min(5).trim().max(255).required(),
     password: Joi.string().min(5).trim().required()
   };
   return Joi.validate(user, schema);
