43b87144d244698e95832e73b39c504b0380e868
Samuel George <samuegeorge58@gmail.com>
2018-08-03 09:24:35 +0100

    [Feature #159493401] Remove staus failed response

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken';
 
 export default (req, res, next) => {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).json({ message: 'Access denied, no token provided', status: 'Failed' });
+  if (!token) return res.status(401).json({ message: 'Access denied, no token provided'});
 
   try {
     const decoded = jwt.verify(token, 'oiraid');
     req.user = decoded;
     next();
   } catch (exception) {
-    res.status(400).json({ message: 'Invalid token', status: 'Failed' });
+    res.status(400).json({ message: 'Invalid token'});
   }
 };
